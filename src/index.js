3d3de43cbcd6156248ffc078af5afaea11b9d863
shahids444 <shahidshaiks444@gmail.com>
2025-07-30 23:18:38 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+// src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './components/App';
+import './styles.css'; // Your CSS moved here
 
-
-
-ReactDOM.render(<App />, document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
